0a9fdda692d4093c9e95841c4387737b055a032d
MateusAciole <devmateusabf@gmail.com>
2024-11-11 17:20:04 -0300

    Project Created

---
+++ src/test/java/com/devmatt/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmatt.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
