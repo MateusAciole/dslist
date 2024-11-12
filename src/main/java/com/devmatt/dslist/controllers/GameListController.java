0d5f508b46aed1bdf9fe606d9261b6a58bbbdac8
MateusAciole <devmateusabf@gmail.com>
2024-11-12 17:10:26 -0300

    Get Lists

---
+++ src/main/java/com/devmatt/dslist/controllers/GameListController.java
@@ -0,0 +1,26 @@
+package com.devmatt.dslist.controllers;
+
+import com.devmatt.dslist.dto.GameListDTO;
+import com.devmatt.dslist.dto.GameMinDTO;
+import com.devmatt.dslist.services.GameListService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/lists")
+public class GameListController {
+
+    @Autowired
+    private GameListService gameListService;
+
+
+    @GetMapping
+    public List<GameListDTO> findAll(){
+        List<GameListDTO> result = gameListService.findAll();
+        return result;
+    }
+}
---
+++ src/main/java/com/devmatt/dslist/repositories/GameListRepository.java
@@ -0,0 +1,8 @@
+package com.devmatt.dslist.repositories;
+
+import com.devmatt.dslist.entities.GameList;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GameListRepository extends JpaRepository<GameList, Long> {
+
+}
